8737fb207a49861ae5160af98d5b326afdafff7e
lebedev-maxim-dmitrievich <  lebedevmaximdmitrievich@gmail.com>
2021-04-24 20:06:16 +0300

    методы возвращают сохраненные объекты

--- src/main/java/ru/lebedev/servicecars/controller/CarController.java
+++ src/main/java/ru/lebedev/servicecars/controller/CarController.java
@@ -2,10 +2,10 @@ package ru.lebedev.servicecars.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.lebedev.servicecars.exception.GetMethodException;
 import ru.lebedev.servicecars.exception.PostMethodException;
 import ru.lebedev.servicecars.exception.PutMethodException;
-import ru.lebedev.servicecars.model.CarDTO;
+import ru.lebedev.servicecars.model.Car;
+import ru.lebedev.servicecars.dto.CarDTO;
 import ru.lebedev.servicecars.repository.CarRepository;
 
 import java.util.Optional;
@@ -21,51 +21,49 @@ public class CarController {
     }
 
     @PostMapping
-    public Optional<CarDTO> addCar(@RequestBody CarDTO body) {
-        if (body != null) {
-            CarDTO carDTO = body;
-            if (checkDataCarForEmptiness(carDTO)) {
-                carRepository.save(carDTO);
+    public Car addCar(@RequestBody CarDTO carDTO) {
+        Car car = new Car();
+        car.setBrande(carDTO.getBrande());
+        car.setCost(carDTO.getCost());
+        car.setMileage(carDTO.getMileage());
+        car.setModel(carDTO.getModel());
+        car.setYearProduction(carDTO.getYearProduction());
 
-                return carRepository.findById(carDTO.getId());
-            }
+        if (checkDataCarForEmptiness(car)) {
+            return carRepository.save(car);
         }
         throw new PostMethodException();
     }
 
     @PutMapping
-    public Optional<CarDTO> updateCar(@RequestBody CarDTO body) {
-        if (body != null) {
-            CarDTO carDTO = body;
-            if (checkDataCarForEmptiness(carDTO)) {
-                carRepository.save(carDTO);
-                return carRepository.findById(carDTO.getId());
-            }
+    public Car updateCar(@RequestBody CarDTO carDTO) {
+        Car car = new Car();
+        car.setBrande(carDTO.getBrande());
+        car.setCost(carDTO.getCost());
+        car.setMileage(carDTO.getMileage());
+        car.setModel(carDTO.getModel());
+        car.setYearProduction(carDTO.getYearProduction());
+        if (checkDataCarForEmptiness(car)) {
+            return carRepository.save(car);
         }
         throw new PutMethodException();
     }
 
     @GetMapping
-    public Optional<CarDTO> getCar(@RequestBody CarDTO body) {
-        if (body != null) {
-            CarDTO carDTO = body;
-            return carRepository.findById(carDTO.getId());
-        }
-        throw new GetMethodException();
+    public Optional<Car> getCar(@RequestBody CarDTO carDTO) {
+        return carRepository.findById(carDTO.getId());
     }
 
     @DeleteMapping
-    public boolean deleteCar(@RequestBody CarDTO body) {
-        if (body != null) {
-            CarDTO carDTO = body;
-            carRepository.deleteById(carDTO.getId());
-            return true;
-        }
-        return false;
+    public boolean deleteCar(@RequestBody CarDTO carDTO) {
+        Car car = new Car();
+        car.setId(carDTO.getId());
+        carRepository.deleteById(car.getId());
+        return true;
     }
 
-    private boolean checkDataCarForEmptiness(CarDTO carDTO) {
-        return carDTO.getBrande() != null && carDTO.getModel() != null && !(carDTO.getCost() <= 0.0) &&
-                !carDTO.getBrande().equals("") && !carDTO.getModel().equals("");
+    private boolean checkDataCarForEmptiness(Car car) {
+        return car.getBrande() != null && car.getModel() != null && !(car.getCost() <= 0.0) &&
+                !car.getBrande().equals("") && !car.getModel().equals("");
     }
 }
