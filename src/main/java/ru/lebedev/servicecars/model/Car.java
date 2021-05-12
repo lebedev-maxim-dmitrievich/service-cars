1851a887c06ec411b8e599b29140997c0fcd2652
lebedev-maxim-dmitrievich <  lebedevmaximdmitrievich@gmail.com>
2021-05-12 19:46:47 +0300

    изменил способ генерации ID

--- src/main/java/ru/lebedev/servicecars/model/Car.java
+++ src/main/java/ru/lebedev/servicecars/model/Car.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Car {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "brande")
     private String brande;
