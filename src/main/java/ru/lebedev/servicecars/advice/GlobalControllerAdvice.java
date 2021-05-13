55b6a94720cadd3d9746302cf2ab2cd0098a56df
lebedev-maxim-dmitrievich <  lebedevmaximdmitrievich@gmail.com>
2021-05-13 18:50:14 +0300

    изменил ExceptionHandler только для кастомных Exception'ов

--- src/main/java/ru/lebedev/servicecars/advice/GlobalControllerAdvice.java
+++ src/main/java/ru/lebedev/servicecars/advice/GlobalControllerAdvice.java
@@ -4,13 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.lebedev.servicecars.exception.CarNotFoundException;
+import ru.lebedev.servicecars.exception.NumberNotCorrectException;
+import ru.lebedev.servicecars.exception.OldCarException;
 import ru.lebedev.servicecars.response.ErrorResponse;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-    @ExceptionHandler
-    public ResponseEntity<?> exceptionHandler(Exception e) {
+    @ExceptionHandler({CarNotFoundException.class, NumberNotCorrectException.class, OldCarException.class})
+    public ResponseEntity<?> carSuitabilityExceptionHandler(Exception e) {
         ErrorResponse response = new ErrorResponse();
         response.setMessage(e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
